7d7abf950f78cfce61b32b940edf1e1af8d4e025
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-09 09:08:19 +0200

    form handling

--- src/awt_demo/HelloGui.java
+++ src/awt_demo/HelloGui.java
@@ -1,40 +1,78 @@
 package awt_demo;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.Normalizer;
+
 
 /** The type Hello gui. */
+
+
 public class HelloGui extends Frame {
-  /** Instantiates a new Hello gui. */
+  Label status = null;
+  Button send = null;
+  Button clear = null;
+  TextField text = null;
+
+  class ButtonClickEventHandler implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      if(e.getActionCommand().equals("SEND")) {
+        status.setText("Sending: " + text.getText());
+      }else if(e.getActionCommand().equals("CLEAR")) {
+        text.setText("");
+        status.setText("");
+      }
+    }
+  }
   public HelloGui() {
-    // getting the screen size
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int screenWidth = (int) screenSize.getWidth();
-    int screenHeight = (int) screenSize.getHeight();
-    this.setBounds(screenWidth - 296, 2, 300, 200);
-    this.setTitle("AWT Demo");
+    this.setBounds(0, 0, 1366, 768);
+    this.setTitle("Application");
     this.setVisible(true);
-    this.setBackground(Color.BLACK);
+    this.setBackground(Color.decode("#0f172a"));
     this.setForeground(Color.WHITE);
-    Label label = new Label("AWT Demo Application");
-    label.setFont(new Font("Operator Mono Medium", Font.BOLD, 20));
-    label.setBounds(screenWidth - 296, 5, 1000, 80);
-    this.add(label);
+    Panel p1 = new Panel();
+    Panel p2 = new Panel();
+    Panel p3 = new Panel();
+    p1.setBounds(0, 0, 1366, 100);
+    p2.setBounds(0, 100, 1366, 568);
+    p3.setBounds(0, 668, 1366, 100);
+    this.setLayout(null);
+    this.add(p1);
+    this.add(p2);
+    this.add(p3);
+    Label normal = new Label();
+    text = new TextField();
+    send = new Button("SEND");
+    clear = new Button("CLEAR");
+    status = new Label("Status");
+    send.setActionCommand("SEND");
+    text.setBounds(0, 0, 300, 30);
+    send.setBounds(0, 40, 100, 30);
+    clear.setBounds(100, 40, 100, 30);
+    status.setBounds(0, 80, 300, 30);
+    clear.setActionCommand("CLEAR");
+    send.addActionListener(new ButtonClickEventHandler());
+    clear.addActionListener(new ButtonClickEventHandler());
+    p2.add(send);
+    p2.add(clear);
+    p2.add(text);
+    p3.add(status);
     this.addWindowListener(
         new WindowAdapter() {
           public void windowClosing(WindowEvent e) {
-            label.setText("Window is closing");
-            label.setFont(new Font("OPERATOR MONO MEDIUM", Font.BOLD, 20));
+            normal.setText("Window is closing");
             try {
-              Thread.sleep(1000);
+              Thread.sleep(10);
             } catch (InterruptedException ex) {
               ex.printStackTrace();
             }
             dispose();
           }
         });
-    this.setLayout(null);
   }
 
   /**
@@ -46,4 +84,5 @@ public class HelloGui extends Frame {
   public static void main(String[] args) {
     new HelloGui();
   }
+
 }
